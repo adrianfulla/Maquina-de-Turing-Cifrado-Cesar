70ef1a687969842a3924a27d3ca8b52f5783d469
Adrian Fulladolsa <ful21592@uvg.edu.gt>
2023-11-19 00:02:11 -0600

    decifrado

--- Main.py
+++ Main.py
@@ -2,12 +2,14 @@ from bin.TuringMachine import TuringMachine
 from bin.JsonOperator import JsonOperator
             
      
-def main():
+def cypher(n, expression):
     jsonOp = JsonOperator()
-    data = jsonOp.readJson("bin/turingmachine.json")
+    data = jsonOp.readJson("bin/turingmachinecypher.json")
+    
+    expression = expression.upper().replace(" ", "_")
 
     tapes = (
-        "__1THE_QUICK_BROWN_FOX_JUMPS_OVER_THE_LAZY_DOG__",
+        f"__{str(n)}{expression}__",
         "*ABCDEFGHIJKLMNOPQRSTUVWXYZ_"
     )
     
@@ -18,14 +20,50 @@ def main():
         for simbols in data['transitions'][state]:
             transitions[state][tuple(simbols.replace("(", "").replace(")", "").replace("'", "").split(","))] = data['transitions'][state][simbols]
     
-    print(data['input_alphabet'])
     
     MT = TuringMachine((tapes[0], tapes[1],tapes[1]), data['states'], 
                        data['input_alphabet'], data['tape_alphabet'],
                        data['initial_state'], data['accept_states'],
                        transitions)
     
-    MT.run()
+    result = MT.run()
+    result = ''.join(result).strip("_")
+    pres = result.replace("_", " ")
+    print(f"Expresion resultante: {pres}")
+    return result
+    
+def decypher(n, expression):
+    jsonOp = JsonOperator()
+    data = jsonOp.readJson("bin/turingmachinedecypher.json")
+
+    expression = expression.upper().strip("_")
+
+    tapes = (
+        f"__{str(n)}{expression}__",
+        "*ABCDEFGHIJKLMNOPQRSTUVWXYZ_"
+    )
+    
+    transitions = {}
+    
+    for state in data['transitions']:
+        transitions[state] = {}
+        for simbols in data['transitions'][state]:
+            transitions[state][tuple(simbols.replace("(", "").replace(")", "").replace("'", "").split(","))] = data['transitions'][state][simbols]
+    
+    MT = TuringMachine((tapes[0], tapes[1],tapes[1]), data['states'], 
+                       data['input_alphabet'], data['tape_alphabet'],
+                       data['initial_state'], data['accept_states'],
+                       transitions)
+    
+    result = MT.run()
+    result = ''.join(result).strip("_")
+    pres = result.replace("_", " ")
+    print(f"Expresion resultante: {pres}")
+    return result
     
 if __name__ == '__main__':
-    main()
+    n = 25
+    expression = "The quick brown fox jumps over the lazy dog"
+     
+    res = cypher(n=n, expression=expression)
+    deres = decypher(n=n, expression=res)
--- bin/TuringMachine.py
+++ bin/TuringMachine.py
@@ -1,9 +1,8 @@
 from time import sleep
+import os
 class TuringMachine:
     def __init__(self, tapes, states, input_alphabet, tape_alphabet, initial_state, accept_states,transition_function):
-        self.tape1 = list(tapes[0])
-        self.tape2 = list(tapes[1])
-        self.tape3 = list(tapes[2])
+        self.tapes = [list(tapes[0]), list(tapes[1]), list(tapes[2])]
         self.heads = [0,0,0]
         self.state = initial_state
         self.transition_function = transition_function
@@ -12,14 +11,15 @@ class TuringMachine:
         self.tape_alphabet = tape_alphabet
         self.accept_states = accept_states
         self.recursion = 0
+        self.steps = 0
         
         self.n = ["1","2","3","4","5","6","7","8","9",] 
         self.alp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
     def step(self):
-        symbol1 = self.tape1[self.heads[0]]
-        symbol2 = self.tape2[self.heads[1]]
-        symbol3 = self.tape3[self.heads[2]]
+        symbol1 = self.tapes[0][self.heads[0]]
+        symbol2 = self.tapes[1][self.heads[1]]
+        symbol3 = self.tapes[2][self.heads[2]]
         if symbol1 in self.n:
             symbol1 = 'n'
             if symbol2 in self.alp:
@@ -43,30 +43,30 @@ class TuringMachine:
         
         inputS = (symbol1, symbol2, symbol3)
         
-        print(f"input: {inputS}, heads:{self.heads}")
+       # print(f"input: {inputS}, heads:{self.heads}")
         
         transition = []
         
         if inputS in self.transition_function[self.state]:
             transition = list(self.transition_function[self.state][inputS])
-            print(f"transicion: {transition}")
+        #    print(f"transicion: {transition}")
         
 
         if transition:
             new_state, new_symbols, moves = transition
             
             if new_symbols[0] == 'n-1':
-                self.tape1[self.heads[0]] = str(int(self.tape1[self.heads[0]])-1)
+                self.tapes[0][self.heads[0]] = str(int(self.tapes[0][self.heads[0]])-1)
             elif new_symbols[0] == 'z':
-                self.tape1[self.heads[0]] = self.tape3[self.heads[2]]
+                self.tapes[0][self.heads[0]] = self.tapes[2][self.heads[2]]
             elif new_symbols[0] == '9':
-                self.tape1[self.heads[0]] = '9'
+                self.tapes[0][self.heads[0]] = '9'
             elif new_symbols[0] == '_':
-                self.tape1[self.heads[0]] = '_'
+                self.tapes[0][self.heads[0]] = '_'
             if new_symbols[1] == 'x' or new_symbols[1] == 'y':
-                self.tape2[self.heads[1]] = self.tape2[self.heads[1]]
+                self.tapes[1][self.heads[1]] = self.tapes[1][self.heads[1]]
             if new_symbols[2] == 'z':
-                self.tape3[self.heads[2]] = self.tape3[self.heads[2]]
+                self.tapes[2][self.heads[2]] = self.tapes[2][self.heads[2]]
                 
             for i in range(len(self.heads)):
                 if moves[i] == 'R':
@@ -74,13 +74,13 @@ class TuringMachine:
                 elif moves[i] == 'L':
                     self.heads[i] -= 1
 
-            if self.heads[0] == len(self.tape1):
-                self.tape1.append('_')
+            if self.heads[0] == len(self.tapes[0]):
+                self.tapes[0].append('_')
                 self.recursion += 1
-            if self.heads[1] == len(self.tape2):
-                self.tape2.append('_')
-            if self.heads[2] == len(self.tape3):
-                self.tape3.append('_')
+            if self.heads[1] == len(self.tapes[1]):
+                self.tapes[1].append('_')
+            if self.heads[2] == len(self.tapes[2]):
+                self.tapes[2].append('_')
 
             self.state = new_state
             
@@ -88,19 +88,42 @@ class TuringMachine:
                 print("Recursion maxima lograda")
                 return False
             
+            self.steps += 1
+            
             return True
         else:
             print("La máquina se detuvo.")
             return False
 
     def run(self):
+        LINE_UP = '\033[1A'
+        LINE_CLEAR = '\x1b[2K'
         while True:
-            print(f"Estado: {self.state}")
-            print(f"Cinta 1: {''.join(self.tape1)}")
-            print(f"Cinta 2: {''.join(self.tape2)}")
-            print(f"Cinta 3: {''.join(self.tape3)}")
-            print()
+            self.printStatus()
 
             if not self.step():
-                break
+                if self.state in self.accept_states:
+                    print(f"La cadena SI fue aceptada en {self.steps}")
+                else:
+                    print(f"La cadena NO fue aceptada {self.steps}")
+                return self.tapes[0]
             
+            sleep(0.05)
+            
+            
+    def printStatus(self):
+        os.system('clear' if os.name == 'posix' else 'cls')
+        cintas =["","",""]
+        for i in range(len(self.tapes)):
+            for j in range(self.heads[i]):
+                cintas[i] += (self.tapes[i][j])
+            cintas[i] +=(f"[{self.tapes[i][self.heads[i]]}]")
+            for j in range(self.heads[i]+1, len(self.tapes[i])):
+                cintas[i] += (self.tapes[i][j])
+                
+        exp = (f"Estado: {self.state}\nCinta 1: {cintas[0]}\nCinta 2: {cintas[1]}\nCinta 3: {cintas[2]}\n")
+
+        print(exp)
+        
+        
+        
